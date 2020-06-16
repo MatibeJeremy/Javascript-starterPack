969e5d830a6d51a415a76d1138a47c30d9f0dab1
matibejeremy <matibejeremy@gmail.com>
2020-06-16 13:04:05 +0300

    Added Transapilling using Babel and Bundling with Webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,36 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default{
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  mode: 'development',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer:{
+    contentBase: path.resolve(__dirname,'src')
+  },
+  plugins:[],
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  module:{
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      }
+    ]
+  }
+};
