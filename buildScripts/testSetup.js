61614bad0087d2f444faad2bb932b2b7d4683bf5
matibejeremy <matibejeremy@gmail.com>
2020-06-17 10:25:52 +0300

    Fixed Travis CI build

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -1,7 +1,7 @@
 //This file isnt transpiled, so must us Vanilla
 
 //register babel to transpile before our tests run
-require('babel-register')();
+require('@babel/register')();
 
 //Disable Webpack features that Mocha doesn't understand
 require.extensions['.css'] = function() {};
